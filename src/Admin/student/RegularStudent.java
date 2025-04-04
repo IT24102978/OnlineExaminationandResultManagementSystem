01af3fcd13e01c2daa72c1d0e05762f7cd3ff6c4
IT24102978 <it24102978@my.sliit.lk>
2025-04-04 17:33:08 +0530

    Admin V.0.1 MockBackend

--- src/Admin/student/RegularStudent.java
+++
@@ -1,7 +0,0 @@
-package student;
-
-public class RegularStudent extends Student {
-    public RegularStudent(String id, String name, String email, String course) {
-        super(id, name, email, course);
-    }
-}
--- src/Admin/student/ScholarshipStudent.java
+++
@@ -1,7 +0,0 @@
-package student;
-
-public class ScholarshipStudent extends Student {
-    public ScholarshipStudent(String id, String name, String email, String course) {
-        super(id, name, email, course);
-    }
-}
