5f0a38e2f0b68f7582aa094eafc76658bb7ed6ac
IT24102404 <IT24102404@my.sliit.lk>
2025-05-19 09:32:12 +0530

    Update subjectController.java

--- src/main/java/result_management/controllers/subjectController.java
+++ src/main/java/result_management/controllers/subjectController.java
@@ -24,12 +24,12 @@ public class subjectController {
     }
 
 
-    public String subNameById(List<String[]> subjects, String studentId) {
+   public String subNameById(List<String[]> subjects, String SubCode) {
         for (String[] sub : subjects) {
-            if (sub[0].equals(studentId)) { // student[0] is ID
-                return sub[1]; // student[1] is Name
+            if (sub[0].equals(SubCode)) { 
+                return sub[1]; 
             }
         }
-        return null; // Return null if student ID not found
+        return null;
     }
 }
